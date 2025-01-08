d6ebdabb44bfff5ea7a96999f82d2fb5a8c06441
code-with-zulquar <codewithzulquar@gmail.com>
2025-01-08 21:29:50 +0530

    Update

--- src/component/Project.jsx
+++ src/component/Project.jsx
@@ -1,8 +1,20 @@
 
+
+
+import { useEffect, useState } from "react";
 import leftArrow from "../assets/left_arrow.svg";
 import rightArrow from "../assets/right_arrow.svg";
 
 export default function Project() {
+  const [ProjectData, setProjectData] = useState([]);
+  useEffect(() => {
+    fetch("ProjectData.json")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setProjectData(data);
+      });
+  }, []);
   return (
     <div
       className="container py-4 px-6 pt-20 mx-auto my-20 md:px-20 lg:px-32 w-full overflow-hidden "
@@ -30,8 +42,29 @@ export default function Project() {
 
       {/* Project-Slider-Container */}
 
-      <div>
-        <div></div>
+      <div className="overflow-hidden">
+        <div className="flex gap-8 transition-transform duration-500 ease-in-out">
+          {ProjectData.map((data, index) => (
+            <div className="relative flex-shrink-0 w-full sm:w-1/4" key={index}>
+              <img
+                src={data.image}
+                alt={data.title}
+                className="w-full h-auto mb-14"
+              />
+              <div className="absolute left-0 right-0 bottom-5 flex justify-center">
+                <div className=" inline-block shadow-md bg-white w-3/4  px-4 py-2 ">
+                  <h2 className="text-xl font-semibold text-gray-800 ">
+                    {data.title}
+                  </h2>
+                  <p className="text-gray-500 text-sm">
+                    {data.price} <span> </span>
+                    {data.location}
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
